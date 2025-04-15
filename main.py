cbd987ad8e397d7d8034a9823039ed93b71bcf56
Vovede <zarubin.fr@gmail.com>
2025-04-15 20:02:46 +0300

    main file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from urllib import request
+import requests
 
 from forms.user import RegisterForm, LoginForm
 from flask_login import LoginManager, login_user, login_required, logout_user
@@ -18,10 +19,33 @@ login_manager.init_app(app)
 @app.route("/")
 def index():
     db_session.global_init(f"db/WT_users.db")
-    session = db_session.create_session()
-    users = session.query(User).all()
-    names = {name.id: (name.surname, name.name) for name in users}
-    return render_template("index.html", names=names)
+    months = ["Январь", "Февраль", "Март",
+              "Апрель", "Май", "Июнь",
+              "Июль", "Август", "Сентябрь",
+              "Октябрь", "Ноябрь", "Декабрь"]
+
+    WEATHER_API_KEY = "137f14bfa0d3400a8f9183700251404"
+    city = "Москва"
+    url = f"http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={city}&lang=ru"
+    response = requests.get(url)
+    current_weather_data = response.json()
+    date = list(map(int, current_weather_data["current"]["last_updated"].split()[0].split("-")))
+    today_info = [date[2], months[date[1] % 12], date[1]]
+
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={city}&days=14&lang=ru"
+    response = requests.get(url)
+    forecast_weather_data = response.json()["forecast"]["forecastday"]
+
+    forecast_days_info = []
+    for day in forecast_weather_data:
+        date = list(map(int, day["date"].split("-")))
+        forecast_days_info.append([date[2], months[date[1] % 12], date[1]])
+
+    return render_template("index.html",
+                           today_info=today_info,
+                           forecast_days_info=forecast_days_info,
+                           current_weather_data=current_weather_data,
+                           forecast_weather_data=forecast_weather_data)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -39,7 +63,8 @@ def reqister():
                                    message="Такой пользователь уже есть")
         user = User(
             name=form.name.data,
-            email=form.email.data
+            email=form.email.data,
+            home_city=form.home_city.data
         )
         user.set_password(form.password.data)
         db_sess.add(user)
