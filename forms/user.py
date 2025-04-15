800dd5c819a7a6c71d1f1295363ba4d0f27b7b78
Vovede <zarubin.fr@gmail.com>
2025-04-15 20:02:33 +0300

    Login/Register form

--- forms/user.py
+++ forms/user.py
@@ -8,6 +8,7 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
+    home_city = StringField('Ваш город', validators=[DataRequired()])
     submit = SubmitField('Войти')
 
 
