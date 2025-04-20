069809bb0f3abe77852ebde47385469fa23e863d
Vovede <zarubin.fr@gmail.com>
2025-04-20 17:24:02 +0300

    main file

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from data.users import User
 
 from flask import Flask, render_template, redirect, make_response, session
 
+from datetime import datetime
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
@@ -19,33 +21,40 @@ login_manager.init_app(app)
 @app.route("/")
 def index():
     db_session.global_init(f"db/WT_users.db")
-    months = ["Январь", "Февраль", "Март",
-              "Апрель", "Май", "Июнь",
-              "Июль", "Август", "Сентябрь",
-              "Октябрь", "Ноябрь", "Декабрь"]
+    months = ["Января", "Февраля", "Марта",
+              "Апреля", "Мая", "Июня",
+              "Июля", "Августа", "Сентября",
+              "Октября", "Ноября", "Декабря"]
 
     WEATHER_API_KEY = "137f14bfa0d3400a8f9183700251404"
     city = "Москва"
     url = f"http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={city}&lang=ru"
     response = requests.get(url)
-    current_weather_data = response.json()
-    date = list(map(int, current_weather_data["current"]["last_updated"].split()[0].split("-")))
-    today_info = [date[2], months[date[1] % 12], date[1]]
+    current_weather_data = response.json()["current"]
+    date = list(map(int, current_weather_data["last_updated"].split()[0].split("-")))
+    current_weather_data["localtime"] = " ".join([str(date[2]), str(months[date[1] % 12 - 1])])
+
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={city}&days=1&lang=ru"
+    response = requests.get(url)
+    hourly_forecast_weather_data = response.json()["forecast"]["forecastday"][0]["hour"]
+    now = datetime.now().hour
+    for hour in hourly_forecast_weather_data:
+        hour["time"] = hour["time"].split()[1]
+
+    hourly_forecast_weather_data = hourly_forecast_weather_data[now:]
 
     url = f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={city}&days=14&lang=ru"
     response = requests.get(url)
-    forecast_weather_data = response.json()["forecast"]["forecastday"]
+    daily_forecast_weather_data = response.json()["forecast"]["forecastday"]
 
-    forecast_days_info = []
-    for day in forecast_weather_data:
+    for day in daily_forecast_weather_data:
         date = list(map(int, day["date"].split("-")))
-        forecast_days_info.append([date[2], months[date[1] % 12], date[1]])
+        day["date"] = " ".join([str(date[2]), str(months[date[1] % 12 - 1])])
 
     return render_template("index.html",
-                           today_info=today_info,
-                           forecast_days_info=forecast_days_info,
                            current_weather_data=current_weather_data,
-                           forecast_weather_data=forecast_weather_data)
+                           daily_forecast_weather_data=daily_forecast_weather_data,
+                           hourly_forecast_weather_data=hourly_forecast_weather_data)
 
 
 @app.route('/register', methods=['GET', 'POST'])
